eda1fa2f83e07f105023380a2a3ea8363a22f74a
pairofAces <karanchauhan16@gmail.com>
2020-09-26 17:35:06 -0400

    FAQS Accordion Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {JumbotronContainer} from './containers/jumbotron';
 import {FooterContainer} from './containers/footer';
+import { FaqsContainer } from './containers/faqs';
 
 
 function App() {
   return (
   <>
   <JumbotronContainer />
+  <FaqsContainer />
   <FooterContainer />
   </>
   );
