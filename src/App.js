0f6c547758a79066fd97546c1491ca35f42f6e7b
pairofAces <karanchauhan16@gmail.com>
2020-09-26 20:48:12 -0400

    working on routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Route, Switch } from 'react-dom';
 import {JumbotronContainer} from './containers/jumbotron';
 import {FooterContainer} from './containers/footer';
 import { FaqsContainer } from './containers/faqs';
-
+import * as ROUTES from './constants/routes';
 
 function App() {
   return (
