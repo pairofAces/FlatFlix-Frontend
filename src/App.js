4345d41a4da7adec36b207c417cb13af3e1b8bf3
pairofAces <karanchauhan16@gmail.com>
2020-09-26 21:59:32 -0400

    fixed npm problem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-dom';
+import { Route, Switch } from 'react-router';
 import {JumbotronContainer} from './containers/jumbotron';
 import {FooterContainer} from './containers/footer';
 import { FaqsContainer } from './containers/faqs';
