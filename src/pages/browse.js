b92e332f5491903c700d0d57c26ab0f86a176d32
pairofAces <karanchauhan16@gmail.com>
2020-09-30 09:47:58 -0400

    working on the browse

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
-export default function Browse() {
-  return <p>Hello from the browse page!</p>;
+// export default function Browse() {
+//   return <p>Hello from the browse page!</p>;
+// }
+
+export default class Browse extends React.Component {
+  state = {
+    currentUser: this.user
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3000/api/v1/movies")
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  }
+
+  render() {
+    return <p>Hello from the browse page!</p>;
+  }
 }
